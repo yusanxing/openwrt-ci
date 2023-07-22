bb9257f0e72a109b85732670656f5fea4d9f5cee
yusanxing <32813254+yusanxing@users.noreply.github.com>
2023-07-22 23:32:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,22 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' >> feeds.conf.default
+#echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+
+#========================================================================================================
+#git clone https://github.com/kongfl888/luci-app-adguardhome.git package/lean/adguardhome
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/jerrykuku-jd-dailybonus
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
+
+#git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
+#git clone https://github.com/project-lede/luci-app-godproxy.git package/lean/godproxy
+#========================================================================================================
+#git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/lean/luci-app-autotimeset
+#git clone https://github.com/riverscn/openwrt-iptvhelper.git package/lean/iptvhelper
+#git clone https://github.com/NateLol/luci-app-beardropper.git package/lean/beardropper
+#git clone https://github.com/destan19/OpenAppFilter.git package/lean/OpenAppFilter
+#https://github.com/jerrykuku/luci-theme-argon
