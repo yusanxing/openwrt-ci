25f9d667b858819202b7064cbe95592515de90cb
yusanxing <32813254+yusanxing@users.noreply.github.com>
2023-07-23 22:16:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,8 +18,10 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-
 #========================================================================================================
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+git clone https://github.com/kiddin9/openwrt-packages/luci-app-ddnsto.git package/ddnsto
+
 #git clone https://github.com/kongfl888/luci-app-adguardhome.git package/lean/adguardhome
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/jerrykuku-jd-dailybonus
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
