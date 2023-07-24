3fc50585a20a18a251e8ec84699ef57fe931b138
yusanxing <32813254+yusanxing@users.noreply.github.com>
2023-07-24 11:42:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,6 +22,12 @@
 
 #========================================================================================================
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+ #git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+ # git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
+
+# feeds获取源码：
+src-git autotimeset https://github.com/sirpdboy/luci-app-autotimeset
+src-git advanced https://github.com/sirpdboy/luci-app-advanced
 
 
 #git clone https://github.com/kongfl888/luci-app-adguardhome.git package/lean/adguardhome
