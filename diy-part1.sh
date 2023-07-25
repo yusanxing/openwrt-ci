65f937934a4151dca8579dcb6bc9a21b51659b5e
yusanxing <32813254+yusanxing@users.noreply.github.com>
2023-07-25 22:53:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,17 +21,14 @@ echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' 
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 #========================================================================================================
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-zerotier package/luci-app-zerotier
+#svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-zerotier package/luci-app-zerotier
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+#git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 #svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci package/openwrt-passwalll
-
 #svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-passwall package/luci-app-passwall
 #svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-turboacc package/luci-app-turboacc
-
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-mosdns package/luci-app-mosdns
-
-#git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 #svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-parentcontrol package/luci-app-parentcontrol
 #svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-ddnsto package/luci-app-ddnsto
 #svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
@@ -41,12 +38,3 @@ git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 #svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-autotimeset package/luci-app-autotimeset
 #svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-socat package/luci-app-socat
 
-#git clone https://github.com/kongfl888/luci-app-adguardhome.git package/lean/adguardhome
-#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
-
-#========================================================================================================
-#git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/lean/luci-app-autotimeset
-#git clone https://github.com/riverscn/openwrt-iptvhelper.git package/lean/iptvhelper
-#git clone https://github.com/NateLol/luci-app-beardropper.git package/lean/beardropper
-#git clone https://github.com/destan19/OpenAppFilter.git package/lean/OpenAppFilter
-#https://github.com/jerrykuku/luci-theme-argon
