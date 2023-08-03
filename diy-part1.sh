42c5cfec663b19ac649ba030258291fc49d7a36b
yusanxing <32813254+yusanxing@users.noreply.github.com>
2023-08-03 10:09:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,14 +20,15 @@ echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.
 
 #========================================================================================================
 # git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
-# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 # rm -rf feeds/packages/net/v2ray-geodata
 # git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 # git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
+# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
 # git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-turboacc package/luci-app-turboacc
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd
 #svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-ddnsto package/luci-app-ddnsto
